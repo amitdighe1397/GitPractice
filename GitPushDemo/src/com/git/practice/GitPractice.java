fdba989f2e30f2ca2e0868487069f9fad7cb742a
amitdighe1397 <amit@1397>
2024-05-13 15:18:48 +0530

    add addition method

--- GitPushDemo/src/com/git/practice/GitPractice.java
+++ GitPushDemo/src/com/git/practice/GitPractice.java
@@ -1,9 +1,17 @@
 package com.git.practice;
 
-public class GitPractice 
-{
-	public static void main(String[] args) 
-	{
+public class GitPractice {
+
+	public static void addition() {
+
+		System.out.println(10 + 20);
+
+	}
+
+	public static void main(String[] args) {
 		System.out.println("Hello...Git...");
+
+		GitPractice.addition();
+
 	}
 }
